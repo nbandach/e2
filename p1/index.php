7a2804a431e5e5579ae2fcd8537b4d444aef93dd
Natalia Bandach <natalia.bandach@gmail.com>
2019-09-23 19:45:19 +0200

    Project 1 delivery.

---
+++ p1/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang='en'>
+<head>
+	<title>Practice Application</title>
+	<meta charset='utf-8'>
+</head>
+<body>
+	<h1>Natalia Bandach</h1>
+	<img src="/images/nat.jpg">
+	<h2>About me</h2>
+	<h3>Digital business specialist, with + 8 years of experience in inbound marketing and growth strategies.</h3>
+    <p>Partner and consultant at KudzuPartners, enabling SMEs to compete though custom technological solutions of big enterprises, helping with growth, scalability and process optimisation.</p>
+	<p>Personal interest: martial arts. Training BJJ, Krav Maga and Kyokushin Karate</p>
+</body>
+</html>
