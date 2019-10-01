ff286a5b163e17a3b313a7ff03f3cdf60808a88b
Natalia Bandach <natalia.bandach@gmail.com>
2019-10-01 21:29:48 +0200

    Started p2

---
+++ p2/index.php
@@ -0,0 +1,15 @@
+<?php require 'index-controller.php'; ?>
+<!doctype html>
+<html lang= 'en'>
+<head>
+    <meta chartset='utf-8'>
+
+</head>
+
+<body>
+    <h1> Coin flip </h1>
+    <h2> Mechanics </h2>
+    <h2> Results </h2>
+
+</body>
+</html>
